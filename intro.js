994dcb3098886f2f68713fb411d50ec9497ca085
ProgrammingRob <rob.g.f.wallace@gmail.com>
2022-11-09 13:45:08 +1100

    added files from vs

---
+++ intro.js
@@ -0,0 +1,22 @@
+/*
+
+Rob
+Learning about JS
+
+*/
+
+//variables
+var first = 0;
+var str = "quote"
+var bool = true;
+var First = 0;
+
+var num;
+
+first = 3;
+console.log(first);
+
+
+
+const pi = 3.14;
+pi = 3.07
